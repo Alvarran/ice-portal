5bc0a85aafb115328d4405ca5d668957122de598
notadrian lol <95841751+Alvarran@users.noreply.github.com>
2022-07-11 20:55:40 +0000

    Initial commit

---
+++ assets/script.js
@@ -0,0 +1,21 @@
+function darkMode() {
+	var element = document.body;
+	element.className = "dark-mode";
+  document.getElementById("1").className = "dark";
+  document.getElementById("2").className = "dark";
+}
+
+function lightMode() {
+	var element = document.body;
+	element.className = "light-mode";
+  document.getElementById("1").className = "light";
+  document.getElementById("2").className = "light";
+}
+
+function change(checkboxElem) {
+  if (checkboxElem.checked) {
+    darkMode();
+  } else {
+    lightMode();
+  }
+}
